dcdee4c3f3c01d21c7bc208474e3413dbecc0e77
Fabrizio <fabrizio.finta@gmail.com>
2017-12-15 13:39:23 +0100

    feat(Main)
    Empty todo case added. Exception handleing at -c + index arg. Invaild command handleing

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,12 @@ public class Main {
                                  " -a   Adds a new task\n" +
                                  " -r   Removes an task\n" +
                                  " -c   Completes an task");
+    } else if ((todo.getElementList().size() == 0)) {
+       System.out.println("No todos for today! :)\n" +
+                          "Add some with '-a' command!");
     } else if (args[0].contains("-l")) {
-      if (! (todo.getElementList().size() == 0)) {
         todo.addOrder();
         todo.printList();
-      } else {
-        System.out.println("No todos for today! :)");
-      }
     } else if ((args[0].contains("-a"))){
         if (args.length == 2) {
         todo.addTodo(args[1]);
@@ -29,8 +28,12 @@ public class Main {
         }
     } else if (args[0].contains("-r")){
       try {
-        int index = Integer.parseInt(args[1])-1;
-        todo.removeTodo(index);
+        if (todo.getElementList().size()!=0) {
+          int index = Integer.parseInt(args[1]) - 1;
+          todo.removeTodo(index);
+        } else {
+          System.out.println("You do not have any todos to remove! Add some with -a");
+        }
       } catch (NumberFormatException e) {
         System.err.println("Unable to remove: index is not a number");
       } catch (IndexOutOfBoundsException e) {
@@ -38,21 +41,28 @@ public class Main {
       }
     } else if (args.length == 2 && args[0].contains("-c")){
       try {
-        int index = Integer.parseInt(args[1])-1;
-        todo.addStatus();
-        System.out.println(todo.getElement(index));
+          int index = Integer.parseInt(args[1]) - 1;
+          todo.addStatus();
+          System.out.println(todo.getElement(index));
       } catch (IndexOutOfBoundsException e) {
-        System.out.println("Unable to check: index is out of bound");
+        System.err.println("Unable to check: index is out of bound");
+      } catch (NumberFormatException e) {
+        System.err.println("Unable to check: index is not a number");
       }
-      //TODO if it is nothing there trhows: Unable to check: index is out of bound
-      //TODO if there is nothing at args[1] throws: Unable to check: no index provided
-      //TODO if there is a string or anything instead of number than throws: Unable to check: index is not a number
     } else if (args[0].contains("-c")){
       todo.addStatus();
       todo.printList();
-      //TODO if there is nothing recognized argument then throws: Invaild argument. && throws the command todo's help screen
+    } else {
+      System.err.println("Invaild argument.");
+      System.out.println();
+      System.out.println("Command Line Todo application\n" +
+                                 "=============================\n" +
+                                 "\n" +
+                                 "Command line arguments:\n" +
+                                 " -l   Lists all the tasks\n" +
+                                 " -a   Adds a new task\n" +
+                                 " -r   Removes an task\n" +
+                                 " -c   Completes an task");
     }
-    
-    
   }
 }
